4b5f0b788def50f5a03f581fe24d71369c109a1c
Peineary Development <peinearydevelopment@gmail.com>
2019-07-22 15:44:32 -0400

    test strength pipe

---
+++ src/app/strength/strength.pipe.spec.ts
@@ -0,0 +1,15 @@
+import { StrengthPipe } from './strength.pipe';
+
+describe('StrengthPipe', () => {
+  it('should display weak if strength is 5', () => {
+    const pipe = new StrengthPipe();
+
+    expect(pipe.transform(5)).toEqual('5 (weak)');
+  });
+
+  it('should display strong if strength is 10', () => {
+    const pipe = new StrengthPipe();
+
+    expect(pipe.transform(10)).toEqual('10 (strong)');
+  });
+});
