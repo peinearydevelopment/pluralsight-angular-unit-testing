e858b0db65c5fc5776c72d55994b72fbe9c2d0d6
Peineary Development <peinearydevelopment@gmail.com>
2019-07-22 16:11:59 -0400

    test interaction on service

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -19,6 +19,7 @@ describe('HeroesComponent', () => {
     component = new HeroesComponent(mockHeroService);
   });
 
+  // state test
   describe('delete', () => {
     it('should remove the indicated hero from the heroes list', () => {
       // delete method calls subscribe on the the hero service deteleHero method, so need to mock that and its result out
@@ -30,6 +31,26 @@ describe('HeroesComponent', () => {
       expect(component.heroes.find(hero => hero.id === HEROES[2].id)).toBeUndefined();
       expect(component.heroes.find(hero => hero.id === HEROES[0].id)).not.toBeUndefined();
       expect(component.heroes.find(hero => hero.id === HEROES[1].id)).not.toBeUndefined();
+    });
+
+    // interaction test, asserts that external call is made with expected parameters passed
+    it('should call deleteHero with correct hero', () => {
+      mockHeroService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(mockHeroService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
+    });
+
+    it('should call subscribe on result of deleteHero', () => {
+    const mockDeleteHeroMethod = jasmine.createSpyObj(['subscribe']);
+    mockHeroService.deleteHero.and.returnValue(mockDeleteHeroMethod);
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(mockDeleteHeroMethod.subscribe).toHaveBeenCalledWith();
     });
   });
 });
